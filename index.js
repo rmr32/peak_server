315d0a4e9d2b1b8714b8a264b082fa988b875a9a
rmr32 <68486614+rmr32@users.noreply.github.com>
2023-01-18 06:02:40 -0800

    Updated PORT

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
-const roomModel = require('./models/room');
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 var server = http.createServer(app);
 const Room = require('./models/room');
 var io = require('socket.io')(server);
@@ -22,11 +21,11 @@ io.on("connection", (socket) => {
         console.log(nickname);
 
         try{
-        let room = new Room();
-        let player = {
-            socketID: socket.ID,
-            nickname: nickname,
-            playerType: 'X',
+            let room = new Room();
+            let player = {
+                socketID: socket.ID,
+                nickname: nickname,
+                playerType: 'X',
         };
         room.players.push(player);
         room.turn = player;
@@ -36,48 +35,44 @@ io.on("connection", (socket) => {
         socket.join(roomID);
 
         io.to(roomID).emit("createRoomSuccess", room);
-    } catch(e) {
-        console.log(e);
-    }
+        } catch(e) {
+            console.log(e);
+        }
 
     });
 
-    socket.on('joinRoom', async ({
-        nickname, roomID
-    }) => {
+    socket.on('joinRoom', async ({nickname, roomID}) => {
         try {
             if(!roomID.match(/^[0-9a-fA-F]{24}$/)) {
-        socket.emit('errorOccurred', 'Please enter a valid room ID.');
-        return;
-    }
-    let room = await Room.findById(roomID);
-    if(room.isJoin) {
-        let player = {
-            nickname: nickname,
-            socketID: socket.ID,
-            playerType: '0',
-
-
+                socket.emit('errorOccurred', 'Please enter a valid room ID.');
+            return;
+            }
+            let room = await Room.findById(roomID);
+            if(room.isJoin) {
+                let player = {
+                    nickname: nickname,
+                    socketID: socket.ID,
+                    playerType: '0',
+                }
+                socket.join(roomID);
+                room.players.push(player);
+                room.isJoin = false;
+                room = await room.save();
+                io.to(roomID).emit("joinRoomSuccess", room);
+                io.to(roomID).emit("updatePlayers", room.players);
+                io.to(roomID).emit('updateRoom', room);
+            } else {
+                socket.emit('errorOccurred', 'The game is in progress, try again later.');
+            }
+        } catch (e) {
+            console.log(e);
         }
-        socket.join(roomID);
-        room.players.push(player);
-        room.isJoin = false;
-        room = await room.save();
-        io.to(roomID).emit("joinRoomSuccess", room);
-        io.to(roomID).emit("updatePlayers", room.players);
-        io.to(roomID).emit('updateRoom', room);
-    } else {
-        socket.emit('errorOccurred', 'The game is in progress, try again later.');
-    }
-} catch (e) {
-    console.log(e);
-}
     });
 
     // For Game
     socket.on('tap', async ({index, roomID}) => {
         try{
-            let room = await Room.findById(roomID);
+            let room = await Room.findById(roomID);          
             let choice = room.turn.playerType;
             if(room.turnIndex == 0) {
                 room.turn = room.players[1];
@@ -97,6 +92,7 @@ io.on("connection", (socket) => {
             console.log(e);
         }
     });
+        
     socket.on('winner', async ({winnerSocketID, roomID}) => {
         try{
             let room = await Room.findById(roomID);
@@ -115,12 +111,16 @@ io.on("connection", (socket) => {
     })
 });
 
-mongoose.connect(DB).then(() => {
-    console.log('Connection Successful!');
-}).catch((e) => {
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection successful!");
+  })
+  .catch((e) => {
     console.log(e);
-});
+  });
 
 server.listen(port, "0.0.0.0", () => {
-    console.log('Server started and running on port ' + port);
+  console.log(`Server started and running on port ${port}`);
 });
+
